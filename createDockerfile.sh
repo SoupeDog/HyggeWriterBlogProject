cbc807c080196159091fb912f2d79c0d162efaa4
Xavier <xavierpe@qq.com>
2020-01-10 22:17:25 +0800

    部署前的准备

--- createDockerfile.sh
+++ createDockerfile.sh
@@ -8,6 +8,6 @@ DB_PW=$4
 cat > Dockerfile << END_TEXT
 FROM openjdk:8-jre-alpine
 MAINTAINER Xavier xavierpe@qq.com
-COPY target/HyggeWriter.jar /HyggeWriter.jar
-ENTRYPOINT [ "java", "-jar","-Dhost=${DB_HOST}","-DdbName=${DB_NAME}","-Dac=${DB_USER}","-Dpw=${DB_PW}","-Dspring.profiles.active=prod","/HyggeWriter.jar"]
+COPY Service-Blog/target/*.jar /app.jar
+ENTRYPOINT [ "java", "-jar","-Dhost=${DB_HOST}","-DdbName=${DB_NAME}","-Dac=${DB_USER}","-Dpw=${DB_PW}","-Dspring.profiles.active=prod","/app.jar"]
 END_TEXT
