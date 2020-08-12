b497e781e3b0adf97c83c9ee7c9280f6c4eb7a01
Xavier <xavierpe@qq.com>
2020-08-12 21:23:04 +0800

    回滚

--- createDockerfile.sh
+++ createDockerfile.sh
@@ -9,6 +9,5 @@ cat > Dockerfile << END_TEXT
 FROM openjdk:8-jre-alpine
 MAINTAINER Xavier xavierpe@qq.com
 COPY Service-Blog/target/*.jar /app.jar
-COPY --from=hengyunabc/arthas:latest /opt/arthas /opt/arthas
 ENTRYPOINT [ "java", "-jar","-Dhost=${DB_HOST}","-DdbName=${DB_NAME}","-Dac=${DB_USER}","-Dpw=${DB_PW}","-Dspring.profiles.active=prod","/app.jar"]
 END_TEXT
