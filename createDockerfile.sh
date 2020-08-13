fd5d280d97267e84f992a899d0d58e681b85fc9c
Xavier <xavierpe@qq.com>
2020-08-13 15:10:57 +0800

    修复 DockerFile 生成脚本

--- createDockerfile.sh
+++ createDockerfile.sh
@@ -12,5 +12,5 @@ COPY Service-Blog/target/*.jar /app.jar
 
 ENV JVM_OPTS="-Xmx1344M -Xms1344M -Xmn448M -XX:MaxMetaspaceSize=256M -XX:MetaspaceSize=256M -XX:+UseConcMarkSweepGC -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:+ExplicitGCInvokesConcurrentAndUnloadsClasses -XX:+CMSClassUnloadingEnabled -XX:+ParallelRefProcEnabled -XX:+CMSScavengeBeforeRemark -XX:ErrorFile=/logFile/hs_err_pid%p.log   -Xloggc:/logFile/gc.log -XX:HeapDumpPath=/logFile -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintClassHistogramBeforeFullGC -XX:+PrintClassHistogramAfterFullGC -XX:+PrintGCApplicationConcurrentTime -XX:+PrintGCApplicationStoppedTime -XX:+PrintTenuringDistribution -XX:+PrintHeapAtGC"
 
-ENTRYPOINT java \${JVM_OPTS} -jar -Dhost=${DB_HOST} -DdbName=${DB_NAME} -Dac=${DB_USER} -Dpw=${DB_PW} -Dspring.profiles.active=prod app.jar
+ENTRYPOINT ["-c","java \${JVM_OPTS} -jar -Dhost=${DB_HOST} -DdbName=${DB_NAME} -Dac=${DB_USER} -Dpw=${DB_PW} -Dspring.profiles.active=prod app.jar"]
 !
