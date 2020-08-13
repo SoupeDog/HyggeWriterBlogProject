3c961a3cc2160a5b4bd0f72996ae4fd5a5b30bcc
Xavier <xavierpe@qq.com>
2020-08-13 15:31:02 +0800

    Revert "追加 诊断器环境"
    This reverts commit d0072ca0

--- createDockerfile.sh
+++ createDockerfile.sh
@@ -5,12 +5,9 @@ DB_NAME=$2
 DB_USER=$3
 DB_PW=$4
 
-cat > Dockerfile <<!
-FROM openjdk:8-jdk-alpine
+cat > Dockerfile << END_TEXT
+FROM openjdk:8-jre-alpine
 MAINTAINER Xavier xavierpe@qq.com
 COPY Service-Blog/target/*.jar /app.jar
-
-ENV JVM_OPTS="-Xmx1344M -Xms1344M -Xmn448M -XX:MaxMetaspaceSize=256M -XX:MetaspaceSize=256M -XX:+UseConcMarkSweepGC -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:+ExplicitGCInvokesConcurrentAndUnloadsClasses -XX:+CMSClassUnloadingEnabled -XX:+ParallelRefProcEnabled -XX:+CMSScavengeBeforeRemark -XX:ErrorFile=/logFile/hs_err_pid%p.log   -Xloggc:/logFile/gc.log -XX:HeapDumpPath=/logFile -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintClassHistogramBeforeFullGC -XX:+PrintClassHistogramAfterFullGC -XX:+PrintGCApplicationConcurrentTime -XX:+PrintGCApplicationStoppedTime -XX:+PrintTenuringDistribution -XX:+PrintHeapAtGC"
-
-ENTRYPOINT ["/bin/sh","-c","java \${JVM_OPTS} -jar -Dhost=${DB_HOST} -DdbName=${DB_NAME} -Dac=${DB_USER} -Dpw=${DB_PW} -Dspring.profiles.active=prod app.jar"]
-!
+ENTRYPOINT [ "java", "-jar","-Dhost=${DB_HOST}","-DdbName=${DB_NAME}","-Dac=${DB_USER}","-Dpw=${DB_PW}","-Dspring.profiles.active=prod","/app.jar"]
+END_TEXT
