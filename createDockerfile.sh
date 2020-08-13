34fb1fa1ddbd82baab9ee05f43f941c92aa771a7
Xavier <xavierpe@qq.com>
2020-08-13 17:54:22 +0800

    配置 arthas 环境

--- createDockerfile.sh
+++ createDockerfile.sh
@@ -6,7 +6,7 @@ DB_USER=$3
 DB_PW=$4
 
 cat > Dockerfile << END_TEXT
-FROM openjdk:8-jre-alpine
+FROM openjdk:8-jdk-alpine
 MAINTAINER Xavier xavierpe@qq.com
 COPY Service-Blog/target/*.jar /app.jar
 ENTRYPOINT [ "java", "-jar","-Dhost=${DB_HOST}","-DdbName=${DB_NAME}","-Dac=${DB_USER}","-Dpw=${DB_PW}","-Dspring.profiles.active=prod","/app.jar"]
