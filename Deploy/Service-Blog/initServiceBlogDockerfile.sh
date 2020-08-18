2a73b039ede00a29646256b3cf999ec269887b31
xavier <xavierpe@qq.com>
2020-08-19 01:34:31 +0800

    调整参数 观察 gc

--- Deploy/Service-Blog/initServiceBlogDockerfile.sh
+++ Deploy/Service-Blog/initServiceBlogDockerfile.sh
@@ -10,7 +10,7 @@ FROM openjdk:8-jdk-alpine
 MAINTAINER Xavier xavierpe@qq.com
 COPY *.jar /service-blog.jar
 
-ENV JVM_OPTS="-Xmx100M -Xms100M -Xmn20M -Xloggc:/logFile/gc.log -XX:+ParallelRefProcEnabled -XX:ErrorFile=/logFile/hs_err_pid%p.log -XX:HeapDumpPath=/logFile -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintClassHistogramBeforeFullGC -XX:+PrintClassHistogramAfterFullGC -XX:+PrintGCApplicationConcurrentTime -XX:+PrintGCApplicationStoppedTime -XX:+PrintTenuringDistribution -XX:+PrintHeapAtGC -XX:MaxMetaspaceSize=64M -XX:MetaspaceSize=64M -XX:+UseParallelGC -XX:+UseAdaptiveSizePolicy -XX:MaxGCPauseMillis=100"
+ENV JVM_OPTS="-Xmx70M -Xms70M -Xmn20M -Xloggc:/logFile/gc.log -XX:+ParallelRefProcEnabled -XX:ErrorFile=/logFile/hs_err_pid%p.log -XX:HeapDumpPath=/logFile -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintClassHistogramBeforeFullGC -XX:+PrintClassHistogramAfterFullGC -XX:+PrintGCApplicationConcurrentTime -XX:+PrintGCApplicationStoppedTime -XX:+PrintTenuringDistribution -XX:+PrintHeapAtGC -XX:MaxMetaspaceSize=20M -XX:MetaspaceSize=20M -XX:+UseParallelGC -XX:+UseAdaptiveSizePolicy -XX:MaxGCPauseMillis=100"
 
 ENTRYPOINT ["/bin/sh","-c","java \$JVM_OPTS -jar -Dhost=${DB_HOST} -DdbName=${DB_NAME} -Dac=${DB_USER} -Dpw=${DB_PW} -Dspring.profiles.active=prod service-blog.jar"]
 END_TEXT
