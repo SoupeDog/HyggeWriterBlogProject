c823ffa604e7cf02d86f7fead2e33aeef3b708ca
xavier <xavierpe@qq.com>
2020-08-19 01:00:08 +0800

    调整参数 观察 gc

--- Deploy/Service-Blog/initServiceBlogDockerfile.sh
+++ Deploy/Service-Blog/initServiceBlogDockerfile.sh
@@ -10,7 +10,7 @@ FROM openjdk:8-jdk-alpine
 MAINTAINER Xavier xavierpe@qq.com
 COPY *.jar /service-blog.jar
 
-ENV JVM_OPTS="-Xmx1344M -Xms1344M -Xmn448M -XX:MaxMetaspaceSize=256M -XX:MetaspaceSize=256M -XX:+UseConcMarkSweepGC -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:+ExplicitGCInvokesConcurrentAndUnloadsClasses -XX:+CMSClassUnloadingEnabled -XX:+ParallelRefProcEnabled -XX:+CMSScavengeBeforeRemark -XX:ErrorFile=/logFile/hs_err_pid%p.log   -Xloggc:/logFile/gc.log -XX:HeapDumpPath=/logFile -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintClassHistogramBeforeFullGC -XX:+PrintClassHistogramAfterFullGC -XX:+PrintGCApplicationConcurrentTime -XX:+PrintGCApplicationStoppedTime -XX:+PrintTenuringDistribution -XX:+PrintHeapAtGC"
+ENV JVM_OPTS="-Xmx320M -Xms320M -Xmn64M -Xloggc:/logFile/gc.log -XX:+ParallelRefProcEnabled -XX:ErrorFile=/logFile/hs_err_pid%p.log -XX:HeapDumpPath=/logFile -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintClassHistogramBeforeFullGC -XX:+PrintClassHistogramAfterFullGC -XX:+PrintGCApplicationConcurrentTime -XX:+PrintGCApplicationStoppedTime -XX:+PrintTenuringDistribution -XX:+PrintHeapAtGC -XX:MaxMetaspaceSize=64M -XX:MetaspaceSize=64M -XX:+UseParallelGC -XX:+UseAdaptiveSizePolicy -XX:MaxGCPauseMillis=100"
 
 ENTRYPOINT ["/bin/sh","-c","java \$JVM_OPTS -jar -Dhost=${DB_HOST} -DdbName=${DB_NAME} -Dac=${DB_USER} -Dpw=${DB_PW} -Dspring.profiles.active=prod service-blog.jar"]
 END_TEXT
